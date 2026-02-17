cabddf5da045775ded2a46f0e02c5dcddfbb9f48
Carlosmcms <carlosmcms725@hotmail.com>
2026-02-16 22:09:14 -0500

    Added first dag and check if source file exists task

---
+++ dags/store_dag.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+default_args = {
+  'owner': 'Airflow',
+  'start_date': datetime(2026, 2, 16),
+  'retries': 1,
+  'retry_delay': timedelta(seconds=5)
+}
+
+dag = DAG(
+  'store_dag',
+  default_args = default_args,
+  schedule_interval = '@daily',
+  catchup = False
+)
+
+# Task 1: Check of the source file exists in the input directory location
+t1 = BashOperator(
+  task_id = 'check_file_exists',
+  bash_command = 'shasum ~/store_files_airflow/raw_store_transactions.csv',
+  retries = 2,
+  retry_delay = timedelta(seconds = 15),
+  dag = dag
+)
